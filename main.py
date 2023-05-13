604dfa78b5fb7efdda99d2c20503a9ecdea4e5b1
hellenilda <hellen95limaaraujo@gmail.com>
2023-05-13 19:57:25 -0300

    Classes em Pyhton

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+class Main():
+    def __init__(self):
+        super(Main, self).__init__()
+
+        pg.init()
+
+        pg.display.set_mode((1280,720))
+        pg.display.set_caption('Classes')
+
+
+    def run(self):
+        pass
